18881a58f813c0f1cf2f7eac3dcea845ab58fc23
Antonio Marco da Silva <antomarsi@hotmail.com>
2018-08-22 20:11:01 -0300

    atualização

--- encaixa-pecas/Assets/Scripts/GameManager.cs
+++ encaixa-pecas/Assets/Scripts/GameManager.cs
@@ -55,7 +55,12 @@ public class GameManager : MonoBehaviour {
     private float spawnCooldown = 0f;
     [Tooltip("O numero de blocos até aumentar a velocidade")]
     public int spawnsToSpeedUp;
-    private int spawnsToSpeedUpCount = 0;
+    [Tooltip("Numero do redutor do redutor")]
+    public float redutionReductor;
+    [Tooltip("Pontos necessários para diminuir o redutor")]
+    public int pointToReductReductor;
+    [Tooltip("Redutor minimo")]
+    public float minReductor; 
 
     [Header("Sounds")]
     public AudioSource pointSound;
@@ -83,14 +88,18 @@ public class GameManager : MonoBehaviour {
         if (!gameOver) {
             if (spawnCooldown <= 0) {
                 spawnRandomColorBlock();
-                spawnsToSpeedUpCount++;
-                if(spawnsToSpeedUp == spawnsToSpeedUpCount) {
-                    spawnsToSpeedUpCount = 0;
+                if((score % spawnsToSpeedUp) == 0) {
                     if (spawnTimer > spawnMinTimer) {
                         spawnTimer -= spawnReductor;
                     } else if ( spawnTimer < spawnMinTimer) {
                         spawnTimer = spawnMinTimer;
                     }
+                }
+                if ((score % pointToReductReductor) == 0 && spawnReductor > minReductor) {
+                    spawnReductor -= redutionReductor;
+                }
+                if (spawnReductor < minReductor) {
+                    spawnReductor = minReductor;
                 }
                 spawnCooldown = spawnTimer;
             }
